a869a544e2852a95b8db3a1c91309a5ca1c2bc1c
Chris Thibault <rchristhibault@gmail.com>
2023-04-13 18:14:18 -0400

    Add Users table migration and seeds.

---
+++ backend/db/migrations/20230413213822-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(256)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
