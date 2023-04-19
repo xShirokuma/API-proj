625278c6f68eed7d789e8a4762a8d70d7ef18b65
Chris Thibault <rchristhibault@gmail.com>
2023-04-19 16:06:00 -0400

    add options to table migrations for remote db

--- backend/db/migrations/20230418221404-create-spot.js
+++ backend/db/migrations/20230418221404-create-spot.js
@@ -61,7 +61,8 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         type: Sequelize.DATE
       }
-    });
+    },
+    options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
--- backend/db/migrations/20230419165648-create-booking.js
+++ backend/db/migrations/20230419165648-create-booking.js
@@ -43,7 +43,8 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         type: Sequelize.DATE
       }
-    });
+    },
+    options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Bookings');
